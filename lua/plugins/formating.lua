3db4e6770f265f5f325a48220428882a5be3acb5
bugenzi <amarbugarin@outlook.com>
2025-06-07 23:44:51 +0200

    Update nvim config

---
+++ lua/plugins/formating.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "goimports", "gofmt" },
+        -- Add more languages as needed
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua", -- Lua formatter
+        "goimports", -- Go imports formatter
+        "gofmt", -- Go formatter
+        "prettier", -- JS/TS/JSON/etc formatter
+      },
+    },
+  },
+}
